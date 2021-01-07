73f5340268f6f275bc8ce6a71d9322c46ddd73ec
Toni Samuels <samuelstoniann@gmail.com>
2021-01-07 00:02:04 -0500

    Folder correction attempt 1

--- lighthouse-js-fundamentals/conditionals.js
+++ lighthouse-js-fundamentals/conditionals.js
@@ -8,7 +8,7 @@ if (raining) {
 if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
-//-------------------------------------------------------//
+//-------------------------------------------------------////
 console.log("Now you're ready to go outside!");
 
 const temperature = 25;
